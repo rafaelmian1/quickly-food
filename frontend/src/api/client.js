853708d9783904084a68f51eb1bf115668c2c38c
Rafael Mian <rafaelmian@MacBook-Pro-de-Rafael.local>
2023-02-16 20:35:16 -0300

    build

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const token = localStorage.getItem('token')
-const baseURL = 'http://localhost:3002/api'
-// const baseURL = 'https://quickly-food.rafaelmiandev.com/api'
+// const baseURL = 'http://localhost:3002/api'
+const baseURL = 'https://quickly-food.rafaelmiandev.com/api'
 const headers = { Authorization: `Bearer ${token}` }
 
 export default axios.create({ baseURL, headers })
